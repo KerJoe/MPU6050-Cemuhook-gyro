253a523fb8fdfb9ffa9871a7b36b5e6e14dc4667
KerJoe <40915672+KerJoe@users.noreply.github.com>
2020-12-25 01:09:46 +0400

    Added calibration sketch

---
+++ calibrate/calibrate.ino
@@ -0,0 +1,255 @@
+#include <Wire.h>
+#include "MPU6050.h" // by jrowberg,
+#include "I2Cdev.h"  // https://github.com/jrowberg/i2cdevlib
+
+const uint8_t MPU6050_sda = 4, MPU6050_scl = 5; // MPU6050 I2C GPIO connection
+const uint8_t MPU_addr = 0x68; // I2C address of the MPU-6050
+
+class MPU6050EXT : public MPU6050{
+  public:
+    void CalibrateAccel_gravitySelect(uint8_t Loops, int8_t gravityAxisIndex = -1, bool reverseGravityDirection = false);
+    void GetActiveOffsets(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz);
+  private:
+    void PID_gravitySelect(uint8_t ReadAddress, float kP,float kI, uint8_t Loops, int8_t gravityAxisIndex = -1, bool reverseGravityDirection = false);
+};
+
+MPU6050EXT accgyr;
+
+const uint8_t gyroSens = 2;
+const float gyroLSB = 131.0f / pow(2, gyroSens);
+
+/**
+  @brief      Fully calibrate Accel from ZERO in about 6-7 Loops 600-700 readings
+*/
+void MPU6050EXT::CalibrateAccel_gravitySelect(uint8_t Loops, int8_t gravityAxisIndex, bool reverseGravityDirection) {
+
+  float kP = 0.3;
+  float kI = 20;
+  float x;
+  x = (100 - map(Loops, 1, 5, 20, 0)) * .01;
+  kP *= x;
+  kI *= x;
+  PID_gravitySelect( 0x3B, kP, kI,  Loops, gravityAxisIndex, reverseGravityDirection);
+}
+
+void MPU6050EXT::GetActiveOffsets(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz) {
+  uint8_t AOffsetRegister = (getDeviceID() < 0x38 )? MPU6050_RA_XA_OFFS_H:0x77;
+  int16_t Data[3];
+  //Serial.print(F("Offset Register 0x"));
+  //Serial.print(AOffsetRegister>>4,HEX);Serial.print(AOffsetRegister&0x0F,HEX);
+  //Serial.print(F("\n//           X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro\n//OFFSETS   "));
+  if(AOffsetRegister == 0x06) I2Cdev::readWords(MPU_addr, AOffsetRegister, 3, (uint16_t *)Data);
+  else {
+    I2Cdev::readWords(MPU_addr, AOffsetRegister, 1, (uint16_t *)Data);
+    I2Cdev::readWords(MPU_addr, AOffsetRegister+3, 1, (uint16_t *)Data+1);
+    I2Cdev::readWords(MPU_addr, AOffsetRegister+6, 1, (uint16_t *)Data+2);
+  }
+  //  A_OFFSET_H_READ_A_OFFS(Data);
+  *ax = Data[0];
+  *ay = Data[1];
+  *az = Data[2];
+  I2Cdev::readWords(MPU_addr, 0x13, 3, (uint16_t *)Data);
+  //  XG_OFFSET_H_READ_OFFS_USR(Data);
+  *gx = Data[0];
+  *gy = Data[1];
+  *gz = Data[2];
+}
+
+void MPU6050EXT::PID_gravitySelect(uint8_t ReadAddress, float kP,float kI, uint8_t Loops, int8_t gravityAxisIndex, bool reverseGravityDirection){
+  uint8_t SaveAddress = (ReadAddress == 0x3B)?((getDeviceID() < 0x38 )? 0x06:0x77):0x13;
+
+  int16_t  Data;
+  float Reading;
+  int16_t BitZero[3];
+  uint8_t shift =(SaveAddress == 0x77)?3:2;
+  float Error, PTerm, ITerm[3];
+  int16_t eSample;
+  uint32_t eSum ;
+  Serial.write('>');
+  for (int i = 0; i < 3; i++) {
+    I2Cdev::readWords(MPU_addr, SaveAddress + (i * shift), 1, (uint16_t *)&Data); // reads 1 or more 16 bit integers (Word)
+    Reading = Data;
+    if(SaveAddress != 0x13){
+      BitZero[i] = Data & 1;                     // Capture Bit Zero to properly handle Accelerometer calibration
+      ITerm[i] = ((float)Reading) * 8;
+      } else {
+      ITerm[i] = Reading * 4;
+    }
+  }
+  for (int L = 0; L < Loops; L++) {
+    eSample = 0;
+    for (int c = 0; c < 100; c++) {// 100 PI Calculations
+      eSum = 0;
+      for (int i = 0; i < 3; i++) {
+        I2Cdev::readWords(MPU_addr, ReadAddress + (i * 2), 1, (uint16_t *)&Data); // reads 1 or more 16 bit integers (Word)
+        Reading = Data;
+        if ((ReadAddress == 0x3B)&&(i == gravityAxisIndex)) Reading += reverseGravityDirection ? -16384 : 16384;  //remove Gravity
+        Error = -Reading;
+        eSum += abs(Reading);
+        PTerm = kP * Error;
+        ITerm[i] += (Error * 0.001) * kI;       // Integral term 1000 Calculations a second = 0.001
+        if(SaveAddress != 0x13){
+          Data = round((PTerm + ITerm[i] ) / 8);    //Compute PID Output
+          Data = ((Data)&0xFFFE) |BitZero[i];     // Insert Bit0 Saved at beginning
+        } else Data = round((PTerm + ITerm[i] ) / 4); //Compute PID Output
+        I2Cdev::writeWords(MPU_addr, SaveAddress + (i * shift), 1, (uint16_t *)&Data);
+      }
+      if((c == 99) && eSum > 1000){           // Error is still to great to continue 
+        c = 0;
+        Serial.write('*');
+      }
+      if((eSum * ((ReadAddress == 0x3B)?.05: 1)) < 5) eSample++;  // Successfully found offsets prepare to  advance
+      if((eSum < 100) && (c > 10) && (eSample >= 10)) break;    // Advance to next Loop
+      delay(1);
+    }
+    Serial.write('.');
+    kP *= .75;
+    kI *= .75;
+    for (int i = 0; i < 3; i++){
+      if(SaveAddress != 0x13) {
+        Data = round((ITerm[i] ) / 8);    //Compute PID Output
+        Data = ((Data)&0xFFFE) |BitZero[i]; // Insert Bit0 Saved at beginning
+      } else Data = round((ITerm[i]) / 4);
+      I2Cdev::writeWords(MPU_addr, SaveAddress + (i * shift), 1, (uint16_t *)&Data);
+    }
+  }
+  resetFIFO();
+  resetDMP();
+}
+
+void setup()
+{
+  Serial.begin(74880);
+  
+  Serial.println("Initializing MPU6050");        
+  Wire.begin(MPU6050_sda, MPU6050_scl); // Connect MPU6050 to GPIO pins defined in MPU6050_sda and MPU6050_scl
+  accgyr.initialize();    
+  Serial.println(accgyr.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  accgyr.setFullScaleGyroRange(gyroSens); 
+
+
+  Serial.println("Starting axis finding...");
+  Serial.println("Put controller on a flat surface in horizontal position and send new line"); while (Serial.available() == 0); Serial.read();
+  accgyr.CalibrateAccel_gravitySelect(3); Serial.println();
+    
+  Serial.println("Put the controller on the right side and send new line"); while (Serial.available() == 0); Serial.read();
+  int16_t ign;
+  byte xAIndex, yAIndex, zAIndex; 
+  bool xASign, yASign, zASign; // True is negative
+  int16_t readings0[3]; bool readings0B[3];
+  accgyr.getMotion6(&readings0[0], &readings0[1], &readings0[2], &ign, &ign, &ign);
+  readings0B[0] = abs(readings0[0]) / 16384.0f > 0.5f; Serial.print("0: "); Serial.println(readings0[0] / 16384.0f);
+  readings0B[1] = abs(readings0[1]) / 16384.0f > 0.5f; Serial.print("1: "); Serial.println(readings0[1] / 16384.0f);
+  readings0B[2] = abs(readings0[2]) / 16384.0f > 0.5f; Serial.print("2: "); Serial.println(readings0[2] / 16384.0f);  
+  
+  Serial.println("Put the controller on the back side (with grips pointing downwards and face with buttons and sticks towards you) and send new line"); while (Serial.available() == 0); Serial.read();
+  int16_t readings1[3]; bool readings1B[3];
+  accgyr.getMotion6(&readings1[0], &readings1[1], &readings1[2], &ign, &ign, &ign);
+  readings1B[0] = abs(readings1[0]) / 16384.0f > 0.5f; Serial.print("0: "); Serial.println(readings1[0] / 16384.0f);
+  readings1B[1] = abs(readings1[1]) / 16384.0f > 0.5f; Serial.print("1: "); Serial.println(readings1[1] / 16384.0f);
+  readings1B[2] = abs(readings1[2]) / 16384.0f > 0.5f; Serial.print("2: "); Serial.println(readings1[2] / 16384.0f);
+  // Find Y axis
+  if (readings0B[0] == readings1B[0]) yAIndex = 0;
+  if (readings0B[1] == readings1B[1]) yAIndex = 1;
+  if (readings0B[2] == readings1B[2]) yAIndex = 2;
+  yASign = readings0[yAIndex] < 0;
+  // Find X axis
+  if (readings0B[0] && yAIndex != 0)  xAIndex = 0;
+  if (readings0B[1] && yAIndex != 1)  xAIndex = 1;
+  if (readings0B[2] && yAIndex != 2)  xAIndex = 2;
+  xASign = readings0[xAIndex] < 0;
+  // Find Y axis
+  if (readings1B[0] && yAIndex != 0)  zAIndex = 0;
+  if (readings1B[1] && yAIndex != 1)  zAIndex = 1;
+  if (readings1B[2] && yAIndex != 2)  zAIndex = 2;
+  zASign = readings1[zAIndex] < 0;  
+
+  // Associations:
+  // X <-> Pitch
+  // Y <-> Yaw
+  // Z <-> Roll
+  byte pGIndex, yGIndex, rGIndex;
+  bool pGSign, yGSign, rGSign;
+  pGIndex = xAIndex;
+  yGIndex = yAIndex;
+  rGIndex = zAIndex;
+  pGSign = !xASign;
+  yGSign = yASign;
+  rGSign = zASign;
+  
+  
+  Serial.println("Starting calibration...");
+  Serial.println("Put controller on a flat surface in horizontal position and send new line"); while (Serial.available() == 0); Serial.read();
+  accgyr.CalibrateAccel_gravitySelect(6, yAIndex, yASign);
+  accgyr.CalibrateGyro(6);
+  Serial.println("\nat 600 Readings");
+  accgyr.PrintActiveOffsets();
+  Serial.println();
+  accgyr.CalibrateAccel_gravitySelect(1, yAIndex, yASign);
+  accgyr.CalibrateGyro(1);
+  Serial.println("700 Total Readings");
+  accgyr.PrintActiveOffsets();
+  Serial.println();
+  accgyr.CalibrateAccel_gravitySelect(1, yAIndex, yASign);
+  accgyr.CalibrateGyro(1);
+  Serial.println("800 Total Readings");
+  accgyr.PrintActiveOffsets();
+  Serial.println();
+  accgyr.CalibrateAccel_gravitySelect(1, yAIndex, yASign);
+  accgyr.CalibrateGyro(1);
+  Serial.println("900 Total Readings");
+  accgyr.PrintActiveOffsets();
+  Serial.println();    
+  accgyr.CalibrateAccel_gravitySelect(1, yAIndex, yASign);
+  accgyr.CalibrateGyro(1);
+  Serial.println("1000 Total Readings");
+  int16_t readings[3]; int16_t ignn;
+  accgyr.getMotion6(&readings[0], &readings[1], &readings[2], &ignn, &ignn, &ignn);
+  Serial.println(readings[0] / 16384.0f);
+  Serial.println(readings[1] / 16384.0f);
+  Serial.println(readings[2] / 16384.0f);  
+
+  int16_t offs[6];
+  accgyr.GetActiveOffsets(&offs[0], &offs[1], &offs[2], &offs[3], &offs[4], &offs[5]);
+
+  /*int16_t gyrOff[6];
+  float gyrOffYF =0;
+  Serial.print("Additional Yaw calibration");  
+  for (uint8_t i = 0; i < 10; i++) // Use simple arithmetic mean
+  {
+    accgyr.getMotion6(&gyrOff[0], &gyrOff[1], &gyrOff[2], &gyrOff[3], &gyrOff[4], &gyrOff[5]);
+    gyrOffYF += yGIndex / gyroLSB; // Instead of gyrYI use your actual Yaw axis
+    Serial.print("."); delay(250);
+  }  
+  gyrOffYF /= 10; Serial.println();*/
+
+  Serial.println("Calibration complete! Put these values into User Defined Data section of gyro.ino:");
+  Serial.print("int16_t* swapTable[] =\n{\n");
+  char *swpVars[] = {"  &accXI,", "  &accYI,", "  &accZI,", "  &gyrPI,", "  &gyrYI,", "  &gyrRI,"};
+  Serial.println(xAIndex == 0 ? swpVars[0] : (yAIndex == 0 ? swpVars[1] : swpVars[2]));
+  Serial.println(xAIndex == 1 ? swpVars[0] : (yAIndex == 1 ? swpVars[1] : swpVars[2]));
+  Serial.println(xAIndex == 2 ? swpVars[0] : (yAIndex == 2 ? swpVars[1] : swpVars[2]));
+  Serial.println(pGIndex == 0 ? swpVars[3] : (yGIndex == 0 ? swpVars[4] : swpVars[5]));
+  Serial.println(pGIndex == 1 ? swpVars[3] : (yGIndex == 1 ? swpVars[4] : swpVars[5]));
+  Serial.println(pGIndex == 2 ? swpVars[3] : (yGIndex == 2 ? swpVars[4] : swpVars[5]));  
+  Serial.println("};");
+  Serial.print("bool signTable[] = // If true change sign\n{\n");
+  Serial.println(xASign ? "  true,  // accXI" : "  false, // accXI");
+  Serial.println(yASign ? "  true,  // accYI" : "  false, // accYI");
+  Serial.println(zASign ? "  true,  // accZI" : "  false, // accZI");
+  Serial.println(pGSign ? "  true,  // gyrPI" : "  false, // gyrPI");
+  Serial.println(yGSign ? "  true,  // gyrYI" : "  false, // gyrYI");
+  Serial.println(rGSign ? "  true,  // gyrRI" : "  false, // gyrRI");  
+  Serial.println("};");
+  Serial.print("int16_t offsetTable[] =\n{\n");
+  Serial.print("  "); Serial.print(offs[0]); Serial.println(", // accXI");
+  Serial.print("  "); Serial.print(offs[1]); Serial.println(", // accYI");
+  Serial.print("  "); Serial.print(offs[2]); Serial.println(", // accZI");
+  Serial.print("  "); Serial.print(offs[3]); Serial.println(", // gyrPI");
+  Serial.print("  "); Serial.print(offs[4]); Serial.println(", // gyrYI");
+  Serial.print("  "); Serial.print(offs[5]); Serial.println(", // gyrRI");  
+  Serial.println("};");
+  //Serial.print("float gyrOffYF = "); Serial.print(gyrOffYF, 5); Serial.println(";");
+}
+
+void loop() {}
---
+++ gyro/gyro.ino
@@ -0,0 +1,368 @@
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "MPU6050.h" // by jrowberg,
+#include "I2Cdev.h"  // https://github.com/jrowberg/i2cdevlib
+#include "CRC32.h"   // by bakercp, https://github.com/bakercp/CRC32
+
+WiFiUDP udp;
+uint8_t  udpIn[28];
+uint8_t  udpOut[100];
+
+bool serialPlotting = false; // Change when using serial plotter
+int16_t accXI, accYI, accZI, gyrPI, gyrYI, gyrRI; // Raw integer orientation data
+float   accXF, accYF, accZF, gyrPF, gyrYF, gyrRF; // Float orientation data
+uint32_t dataPacketNumber = 0; // Current data packet count
+// All time variables are in microseconds
+uint32_t dataSendTime; // Current time
+uint32_t dataRequestTime; // Time of the last data request
+const uint32_t dataSendDelay = 4000; // Time between sending data packages
+const uint32_t dataRequestTimeout = 120000000; // Timeout time for data request
+
+/***************************************************************************************
+ * User Defined Data
+****************************************************************************************/
+
+char wifiSSID[] = "********";
+char wifiPass[] = "********";
+uint16_t udpPort = 26760;
+
+const uint8_t MPU6050_sda = 4, MPU6050_scl = 5; // MPU6050 I2C GPIO connection
+const uint8_t MPU_addr = 0x68; // I2C address of the MPU-6050
+
+
+int16_t* swapTable[] =
+{
+  &accZI,
+  &accXI,
+  &accYI,
+  &gyrRI,
+  &gyrPI,
+  &gyrYI,
+};
+bool signTable[] = // If true change sign
+{
+  false, // accXI
+  false, // accYI
+  true,  // accZI
+  true,  // gyrPI
+  false, // gyrYI
+  true,  // gyrRI
+};
+int16_t offsetTable[] =
+{
+  -2818, // accXI
+  2461, // accYI
+  1148, // accZI
+  -47, // gyrPI
+  -72, // gyrYI
+  35, // gyrRI
+};
+
+
+float gyrOffYF = 0; // Optional: Set this value to Gyro Y from PadTest when the gamepad is immobile to remove drift
+
+
+/***************************************************************************************
+ * 
+****************************************************************************************/
+
+MPU6050 accgyr;
+// Gyro sensitivity 0: +/-250 deg/s, 1: +/-500 deg/s, 2: +/-1000 deg/s, 3: +/-2000 deg/s
+// If set too low it will introduce clipping
+// If set too high it will decrease sensitity
+const uint8_t gyroSens = 2;
+const float gyroLSB = 131.0f / pow(2, gyroSens);
+
+// Info package response packet
+uint8_t makeInfoPackage(uint8_t* output, uint8_t numberOfControllers)
+{
+  // Magic server string
+  output[0] = (uint8_t)'D';
+  output[1] = (uint8_t)'S';
+  output[2] = (uint8_t)'U';
+  output[3] = (uint8_t)'S';
+  // Protocol version (1001)
+  output[4] = 0xE9;
+  output[5] = 0x03;
+  // Packet length without header plus the length of event type (4)
+  output[6] = (uint8_t)(4 + numberOfControllers * 12);
+  output[7] = 0;
+  // Zero out CRC32 field
+  output[8] = 0;
+  output[9] = 0;
+  output[10] = 0;
+  output[11] = 0;
+  // Set server id to some value (0)
+  output[12] = 0;
+  output[13] = 0;
+  output[14] = 0;
+  output[15] = 0;
+  // Event type, controller information (0x00100001)
+  output[16] = 0x01;
+  output[17] = 0x00;
+  output[18] = 0x10;
+  output[19] = 0x00;
+  
+  output[20] = 0x00; // Slot of the device we are reporting about (0)
+  output[21] = 0x02; // Slot state, connected (2)
+  output[22] = 0x02; // Device model, full gyro aka DS4 (2)
+  output[23] = 0x02; // Connection type, bluetooth (2). (May be either USB (1) or Bluetooth (2))
+  // MAC address of device (0x000000000001)
+  output[24] = 0x01; 
+  output[25] = 0x00;
+  output[26] = 0x00;
+  output[27] = 0x00;
+  output[28] = 0x00;
+  output[29] = 0x00;
+  // Batery status, full (5)
+  output[30] = 0x05; // ...
+  output[31] = 0x00; // Termination byte
+
+  // Set controllers other than 0 to unconected state
+  for (uint8_t i = 1; i < numberOfControllers; i++)
+  {
+      output[20 + i * 12] = i;    // Slot of the device we are reporting about (i)
+      output[21 + i * 12] = 0x00; // Slot state, not connected (0)
+      output[22 + i * 12] = 0x00; // Device model, not applicable (0)
+      output[23 + i * 12] = 0x00; // Connection type, not applicable (0)
+      // MAC address of device, not applicable (0x000000000000)
+      output[24 + i * 12] = 0x00;
+      output[25 + i * 12] = 0x00;
+      output[26 + i * 12] = 0x00;
+      output[27 + i * 12] = 0x00;
+      output[28 + i * 12] = 0x00;
+      output[29 + i * 12] = 0x00;
+      // Batery status, not applicable (0)
+      output[30 + i * 12] = 0x00; // ...
+      output[31 + i * 12] = 0x00; // Termination byte
+  }
+
+  CRC32 crc; // Caclulate checksum
+  for (uint8_t i = 0; i < 20 + numberOfControllers * 12; i++) crc.update(udpOut[i]);
+  uint32_t Checksum = crc.finalize();
+  memcpy(&output[8], &Checksum, sizeof(Checksum)); // Copy bytes from Checksum to packet array
+  
+  return 20 + numberOfControllers * 12; // Return the number of bytes in packet
+}
+// Data package response packet
+uint8_t makeDataPackage(uint8_t* output,       uint32_t packetCount,  uint32_t timestamp, 
+                        float accellerometerX, float accellerometerY, float accellerometerZ,
+                        float gyroscopePit,    float gyroscopeYaw,    float gyroscopeRol)
+{
+  // Magic server string
+  output[0] = (uint8_t)'D';
+  output[1] = (uint8_t)'S';
+  output[2] = (uint8_t)'U';
+  output[3] = (uint8_t)'S';
+  // Protocol version (1001)
+  output[4] = 0xE9;
+  output[5] = 0x03;
+  // Packet length without header plus the length of event type (4)
+  output[6] = (uint8_t)(80 + 4);
+  output[7] = 0;
+  // Zero out CRC32 field
+  output[8] = 0;
+  output[9] = 0;
+  output[10] = 0;
+  output[11] = 0;
+  // Set server id to some value (0)
+  output[12] = 0;
+  output[13] = 0;
+  output[14] = 0;
+  output[15] = 0;
+  // Event type, controller data (0x00100002)
+  output[16] = 0x02;
+  output[17] = 0x00;
+  output[18] = 0x10;
+  output[19] = 0x00;
+
+  output[20] = 0x00; // Slot of the device we are reporting about (0)
+  output[21] = 0x02; // Slot state, connected (2)
+  output[22] = 0x02; // Device model, full gyro aka DS4 (2)
+  output[23] = 0x02; // Connection type, bluetooth (2). (May be either USB (1) or Bluetooth (2))
+  // MAC address of device (0x000000000001)
+  output[24] = 0x01; 
+  output[25] = 0x00;
+  output[26] = 0x00;
+  output[27] = 0x00;
+  output[28] = 0x00;
+  output[29] = 0x00;
+  // Batery status, full (5)
+  output[30] = 0x05; // ...
+
+  output[31] = 0x01; // Device state, active (1)
+  memcpy(&udpOut[32], &packetCount, sizeof(packetCount)); // Copy from packetCount to packet array 
+  // We don't care about button, joystick and touchpad data, so we just their bytes to zero.
+  output[36] = 0x00; // D-Pad Left, D-Pad Down, D-Pad Right, D-Pad Up, Options (?), R3, L3, Share (?)
+  output[37] = 0x00; // Y, B, A, X, R1, L1, R2, L2 
+  output[38] = 0x00; // PS Button (unused)
+  output[39] = 0x00; // Touch Button (unused)
+  output[40] = 0x00; // Left stick X (plus rightward)
+  output[41] = 0x00; // Left stick Y (plus upward)
+  output[42] = 0x00; // Right stick X (plus rightward)
+  output[43] = 0x00; // Right stick Y (plus upward)
+  output[44] = 0x00; // Analog D-Pad Left
+  output[45] = 0x00; // Analog D-Pad Down
+  output[46] = 0x00; // Analog D-Pad Right
+  output[47] = 0x00; // Analog D-Pad Up
+  output[48] = 0x00; // Analog Y
+  output[49] = 0x00; // Analog B
+  output[50] = 0x00; // Analog A
+  output[51] = 0x00; // Analog X
+  output[52] = 0x00; // Analog R1
+  output[53] = 0x00; // Analog L1
+  output[54] = 0x00; // Analog R2
+  output[55] = 0x00; // Analog L2
+  output[56] = 0x00; // Is first touch active?
+  output[57] = 0x00; // First touch id
+  output[58] = 0x00; // First touch X (16 bit)
+  output[59] = 0x00; //...
+  output[60] = 0x00; // First touch Y (16 bit)
+  output[61] = 0x00; //...
+  output[62] = 0x00; // Is second touch active?
+  output[63] = 0x00; // Second touch id
+  output[64] = 0x00; // Second touch X (16 bit)
+  output[65] = 0x00; //...
+  output[66] = 0x00; // Second touch Y (16 bit)
+  output[67] = 0x00; //...
+  // Clear 4 higher bytes of timestamp and copy 4 lower bytes of timestamp
+  output[72] = 0x00; 
+  output[73] = 0x00; 
+  output[74] = 0x00; 
+  output[75] = 0x00;
+  memcpy(&output [68], &timestamp, sizeof(timestamp)); // Copy from timestamp to packet array   
+  // Move accelerometer and gyroscope data
+  memcpy(&output [76], &accellerometerX, sizeof(accellerometerX));
+  memcpy(&output [80], &accellerometerY, sizeof(accellerometerY));
+  memcpy(&output [84], &accellerometerZ, sizeof(accellerometerZ));
+  memcpy(&output [88], &gyroscopePit, sizeof(gyroscopePit));
+  memcpy(&output [92], &gyroscopeYaw, sizeof(gyroscopeYaw));
+  memcpy(&output [96], &gyroscopeRol, sizeof(gyroscopeRol));
+
+  CRC32 crc; // Caclulate checksum
+  for (uint8_t i = 0; i < 100; i++) crc.update(output [i]);
+  uint32_t Checksum = crc.finalize();
+  memcpy(&output[8], &Checksum, sizeof(Checksum)); // Copy from Checksum to packet array
+  
+  return 100; // Return the number of bytes in packet
+}
+
+void setup() 
+{
+  Serial.begin(74880);
+
+  Serial.print("\nConnecting");
+  WiFi.begin(wifiSSID, wifiPass);  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }   
+  Serial.print("\nConnected, IP address: ");          
+  Serial.print(WiFi.localIP());
+
+  udp.begin(udpPort);
+  Serial.print("\nUDP server has been set up at port: ");
+  Serial.println(udpPort);  
+  
+  Serial.println("Initializing MPU6050");        
+  Wire.begin(MPU6050_sda, MPU6050_scl); // Connect MPU6050 to GPIO pins defined in MPU6050_sda and MPU6050_scl
+  accgyr.initialize();    
+  Serial.println(accgyr.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+  accgyr.setFullScaleGyroRange(gyroSens); // Set selected gyro sensetivity
+  accgyr.setDLPFMode(4); // Set low pass filter to 20 hz, for noise filtering
+  accgyr.setXAccelOffset(offsetTable[0]);
+  accgyr.setYAccelOffset(offsetTable[1]);
+  accgyr.setZAccelOffset(offsetTable[2]);
+  accgyr.setXGyroOffset(offsetTable[3]);
+  accgyr.setYGyroOffset(offsetTable[4]);
+  accgyr.setZGyroOffset(offsetTable[5]);  
+
+  dataRequestTime = micros(); // Set dataRequestTime, so that if we won't get a data request in time we will shutdown
+
+  Serial.println("Setup done!");     
+}
+
+void loop() 
+{
+  uint8_t packetInSize = udp.parsePacket();
+  if (packetInSize)
+  {
+    udp.read(udpIn, sizeof(udpIn));
+    uint8_t packetOutSize;
+    switch(udpIn[16]) // udpIn[16] - Least significant byte of event type
+    {
+      case 0x01: // Information about controllers
+        Serial.println("Got info request!");
+        
+        packetOutSize = makeInfoPackage(&udpOut[0], udpIn[20]); // udpIn[20] - Amount of ports we should report about
+
+        udp.beginPacket(udp.remoteIP(), udp.remotePort());
+        udp.write(udpOut, packetOutSize);
+        udp.endPacket();        
+      break;
+      case 0x02: // Controller input data
+        Serial.println("Got data request!");      
+        
+        dataRequestTime = micros(); // Refresh timeout timer        
+      break;      
+    }
+  } 
+  if (micros() - dataRequestTime > dataRequestTimeout) // Check if timedout by a lack of controller data requests
+  {    
+    // If we haven't recieved any datapacket in time, 
+    // than orientation information is not needed, so we will shutdown to save energy for the gamepad
+    Serial.println("Shutting down..."); Serial.flush();
+    ESP.deepSleep(0); 
+  }
+  if (micros() - dataSendTime > dataSendDelay) // Check if enough time has elapsed between data packets
+  {        
+    dataPacketNumber++; dataSendTime = micros();
+    
+    accgyr.getMotion6(swapTable[0], swapTable[1], swapTable[2], swapTable[3], swapTable[4], swapTable[5]);  
+
+    // For MPU-6050 the biggest possible number is 32768 and smallest is -32767,
+    // But because it uses 2's complement 16 bit signed numbers the number 32728 is interpreted as -32728
+    // So if we want to prevent the wrap arround from positive to negative we need
+    // to subtract 1 from the raw data
+    accXI--; accYI--; accZI--;
+    gyrPI--; gyrYI--; gyrRI--;      
+ 
+    // Inverse selected axis
+    accXI *= signTable[0] ? -1 : 1;
+    accYI *= signTable[1] ? -1 : 1;
+    accZI *= signTable[2] ? -1 : 1;
+    gyrPI *= signTable[3] ? -1 : 1;
+    gyrYI *= signTable[4] ? -1 : 1;
+    gyrRI *= signTable[5] ? -1 : 1;
+    //gyrYI = -gyrYI;
+    
+    // Convert raw data to float.
+    accXF = accXI / 16384.0f; // Divide by LSB/mg, for 2g sensitivity it is 16384
+    accYF = accYI / 16384.0f;
+    accZF = accZI / 16384.0f;
+
+    gyrPF = gyrPI / gyroLSB;  // Divide by LSB/deg/s
+    gyrYF = gyrYI / gyroLSB;
+    gyrRF = gyrRI / gyroLSB;
+    
+    gyrYF -= gyrOffYF;
+
+    if (serialPlotting) 
+    {
+        Serial.print(" AX: "); Serial.print(accXF);
+        Serial.print(" AY: "); Serial.print(accYF);
+        Serial.print(" AZ: "); Serial.print(accZF);
+        Serial.print(" GP: "); Serial.print(gyrPF);
+        Serial.print(" GY: "); Serial.print(gyrYF);
+        Serial.print(" GR: "); Serial.println(gyrRF);  
+    }
+    
+    uint8_t packetOutSize = makeDataPackage(&udpOut[0], dataPacketNumber, dataSendTime, accXF, accYF, accZF, gyrPF, gyrYF, gyrRF);
+
+    udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    udp.write(udpOut, packetOutSize);
+    udp.endPacket();
+  }
+}
